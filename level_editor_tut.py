29636e44ac46bc2b84c6dec2b4b652b0a1a59c8c
Joakim Nordling <joakim.nordling@gmail.com>
2021-04-03 09:30:27 +0300

    More advanced saving and loading
    Can now read and write to CSV, JSON and PICKLE files.
    You can swap between formats by pressing "f" key, which makes it easy to load in one format, swap format and save to anther format.
    
    Main point was to demonstrate how easy JSON files are to read and write, especially when combined with pathlib.

--- level_editor_tut.py
+++ level_editor_tut.py
@@ -1,5 +1,8 @@
 import csv
+import json
 import pickle
+from enum import Enum
+from pathlib import Path
 
 import pygame
 
@@ -20,6 +23,18 @@ screen = pygame.display.set_mode(
     (SCREEN_WIDTH + SIDE_MARGIN, SCREEN_HEIGHT + LOWER_MARGIN)
 )
 pygame.display.set_caption("Level Editor")
+
+
+class FileFormat(str, Enum):
+    JSON = "json"
+    CSV = "csv"
+    PICKLE = "pickle"
+
+    def next(self):
+        format_list = list(self.__class__)
+        index = format_list.index(self)
+        next_index = (index + 1) % len(format_list)
+        return format_list[next_index]
 
 
 # define game variables
@@ -145,6 +160,7 @@ for i in range(len(img_list)):
         button_row += 1
         button_col = 0
 
+current_file_format = FileFormat.CSV
 
 run = True
 while run:
@@ -155,7 +171,13 @@ while run:
     draw_grid()
     draw_world()
 
-    draw_text(f"Level: {level}", font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
+    draw_text(
+        f"Level: {level}    Format: {current_file_format}",
+        font,
+        WHITE,
+        10,
+        SCREEN_HEIGHT + LOWER_MARGIN - 90,
+    )
     draw_text(
         "Press UP or DOWN to change level",
         font,
@@ -167,27 +189,33 @@ while run:
     # save and load data
     if save_button.draw(screen):
         # save level data
-        with open(f"level{level}_data.csv", "w", newline="") as csvfile:
-            writer = csv.writer(csvfile, delimiter=",")
-            for row in world_data:
-                writer.writerow(row)
-        # alternative pickle method
-        # pickle_out = open(f'level{level}_data', 'wb')
-        # pickle.dump(world_data, pickle_out)
-        # pickle_out.close()
+        file = Path(f"level{level}_data.{current_file_format}")
+        if current_file_format == FileFormat.JSON:
+            file.write_text(json.dumps(world_data))
+        elif current_file_format == FileFormat.CSV:
+            with open(file, "w", newline="") as csvfile:
+                writer = csv.writer(csvfile, delimiter=",")
+                for row in world_data:
+                    writer.writerow(row)
+        elif current_file_format == FileFormat.PICKLE:
+            with open(file, "wb") as pickle_out:
+                pickle.dump(world_data, pickle_out)
     if load_button.draw(screen):
         # load in level data
         # reset scroll back to the start of the level
         scroll = 0
-        with open(f"level{level}_data.csv", newline="") as csvfile:
-            reader = csv.reader(csvfile, delimiter=",")
-            for x, row in enumerate(reader):
-                for y, tile in enumerate(row):
-                    world_data[x][y] = int(tile)
-        # alternative pickle method
-        # world_data = []
-        # pickle_in = open(f'level{level}_data', 'rb')
-        # world_data = pickle.load(pickle_in)
+        file = Path(f"level{level}_data.{current_file_format}")
+        if current_file_format == FileFormat.JSON:
+            world_data = json.loads(file.read_text())
+        elif current_file_format == FileFormat.CSV:
+            with open(file, newline="") as csvfile:
+                reader = csv.reader(csvfile, delimiter=",")
+                for x, row in enumerate(reader):
+                    for y, tile in enumerate(row):
+                        world_data[x][y] = int(tile)
+        elif current_file_format == FileFormat.PICKLE:
+            with open(file, "rb") as pickle_in:
+                world_data = pickle.load(pickle_in)
 
     # draw tile panel and tiles
     pygame.draw.rect(screen, GREEN, (SCREEN_WIDTH, 0, SIDE_MARGIN, SCREEN_HEIGHT))
@@ -237,6 +265,8 @@ while run:
                 scroll_right = True
             if event.key == pygame.K_RSHIFT:
                 scroll_speed = 5
+            if event.key == pygame.K_f:
+                current_file_format = current_file_format.next()
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
